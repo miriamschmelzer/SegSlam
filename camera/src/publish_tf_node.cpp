9ba8af42e1c05a56733731896d74c2121960668d
Miriam Schmelzer <miriamschmelzer@gmx.de>
2019-10-19 15:21:19 +0200

    Tried to publish tf and tf_static simultaneously to tf, didn't work because tf_static was only published when no messages were received from tf anymore

---
+++ camera/src/publish_tf_node.cpp
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <string>
+#include <publisher_subscriber.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <tf2_msgs/TFMessage.h>
+
+template<>
+//void PublisherSubscriber<geometry_msgs::TransformStamped, tf2_msgs::TFMessage>::subscriberCallback(const tf2_msgs::TFMessage::ConstPtr& receivedMsg)
+void PublisherSubscriber<tf2_msgs::TFMessage, tf2_msgs::TFMessage>::subscriberCallback(const tf2_msgs::TFMessage::ConstPtr& receivedMsg)
+{
+  ros::Rate r(10); // 10 hz
+  while (ros::ok())
+  {
+    ROS_INFO("Sending the received message on 'transform' topic");
+    publisherObject.publish(receivedMsg->transforms);
+    /*for(int i=0;i<receivedMsg->transforms.size();i++)
+    {
+      //geometry_msgs::TransformStamped transform_msg;
+      tf2_msgs::TFMessage transform_msg
+      transform_msg = receivedMsg->transforms[i];
+      publisherObject.publish(transform_msg);
+      /*if(receivedMsg->transforms[i].child_frame_id.compare("camera_color_optical_frame") == 0)
+        {
+          geometry_msgs::TransformStamped transform_msg;
+          transform_msg = receivedMsg->transforms[i];
+          publisherObject.publish(transform_msg);
+        }
+
+    }*/
+    r.sleep();
+  }
+
+
+  //geometry_msgs::TransformStamped transform_msg;
+  //geometry_msgs::TransformStamped test = receivedMsg->transforms;
+  /*geometry_msgs::PoseStamped received_pose = receivedMsg->poses.front(); // poses are received in a vector, only the first element is needed, other elements are copies of the first one
+  transform_msg.header = received_pose.header;
+  transform_msg.child_frame_id = "";
+  transform_msg.transform.translation.x = received_pose.pose.position.x;
+  transform_msg.transform.translation.y = received_pose.pose.position.y;
+  transform_msg.transform.translation.z = received_pose.pose.position.z;
+  transform_msg.transform.rotation = received_pose.pose.orientation;
+  */
+  //geometry_msgs::TransformStamped received_transform = receivedMsg->transforms.front();
+  /*if(receivedMsg.transforms.child_frame_id.compare("camera_accel_optical_frame"))
+  {
+    transform_msg = receivedMsg.transforms;
+    publisherObject.publish(transform_msg);
+  }
+  */
+
+}
+
+int main(int argc, char **argv)
+{
+  // Set up ROS
+  ros::init(argc, argv, "publish_tf_node");
+  PublisherSubscriber<tf2_msgs::TFMessage, tf2_msgs::TFMessage> parrot("tf", "tf_static", 1);
+  ros::spin();
+}
